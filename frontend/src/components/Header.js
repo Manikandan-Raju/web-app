1c548381898100c6ebeeab7f0a6e30e798d4d0dc
manikandanraju <itsmanikandanraju@gmail.com>
2023-04-15 18:59:43 +0530

    fix warnings

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,12 +7,10 @@ export default class Header extends Component {
       
       <header id="home">
          <nav id="nav-wrap">
-            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
-          <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
             <ul id="nav" className="nav">
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
-             <li><a className="smoothscroll" href="#resume">Resume</a></li>
+               <li><a className="smoothscroll" href="#resume">Resume</a></li>
                <li><a className="smoothscroll" href="#portfolio">Works</a></li>
                <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
                <li><a className="smoothscroll" href="#contact">Contact</a></li>
@@ -21,7 +19,6 @@ export default class Header extends Component {
 
          <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeData.name}.</h1>
                <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>{resumeData.roleDescription}
                </h3>
                <hr/>
@@ -30,7 +27,7 @@ export default class Header extends Component {
                     resumeData.socialLinks && resumeData.socialLinks.map(item =>{
                       return(
                               <li key={item.name}>
-                                //<a href={item.url} target="_blank" rel="noopener noreferrer" ><i className={item.className}></i></a>
+                                <a href={item.url} target="_blank" rel="noopener noreferrer" ><i className={item.className}></i></a>
                               </li>
                             )
                           }
