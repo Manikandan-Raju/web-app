a8476c18c6031b30790f3a081394c1ea31c40d1e
manikandanraju <itsmanikandanraju@gmail.com>
2023-04-15 18:28:41 +0530

    self-host

---
+++ frontend/src/resumeData.js
@@ -0,0 +1,68 @@
+let resumeData = {
+    "imagebaseurl":"https://manikandan.raju.github.io/",
+    "name": "Manikandan Raju",
+    "role": "Automotive Software Automation Developer",
+    "linkedinId":"https://www.linkedin.com/in/manikandan-raju-a16501138/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Engineering Professional with 4 Years of experience in Automotive Automation Development in ADAS AUTOSAR Domain, SIL & HIL Software Validation and CI/CD DevOps.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/manikandan-raju-a16501138/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Manikandan-Raju",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"> Engineering Professional with 4 Years of experience in Automotive Automation Development in ADAS AUTOSAR Domain, SIL & HIL Software Validation and CI/CD DevOps.\n> Expertise in developing Python automation scripts for ADAS AUTOSAR stack C/C++ code generation , ARXML parsing and Validation of ADAS Functional Requirements.\n> Development of Internal Web Application for Mapping tool of AUTOSAR ports and ADAS Software Components (SwC) using Django RESTAPI and React JS and PostgreSQL Database.\n> Hands-on Experience with dSPACE Simulator-based Hardware in Loop Testing of Engine and Transmission ECU.\n> Hands-on Experience in Requirements Analysis, Test Cases Development and Automation XIL API and CAPL Scripting.",
+    "address":"India",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"The LNM Insitute of Information Technology",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"",
+    "skills":[
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"CSS"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+    ]
+  }
+  
+  export default resumeData
