7e0ad49f83650d1fe0de205ea7bf33115db6ef5d
mani <itsmanikandanraju@gmail.com>
2024-08-18 00:57:33 +0530

    1g

--- frontend/src/components/gold.jsx
+++ frontend/src/components/gold.jsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 function Gold() {
 
     const [GoldRate, setGoldRate] = useState("")
+    const [GoldRate_1g, setGoldRate_1g] = useState("")
 
 
     useEffect(() => {
@@ -20,8 +21,10 @@ function Gold() {
                 const res = await response.json();
                 console.log(res);
                 const goldrate = res["price_gram_22k"] * 800
+                const goldrate_1g = res["price_gram_22k"] * 100
                 console.log(goldrate);
                 setGoldRate(goldrate)
+                setGoldRate_1g(goldrate_1g)
             } catch (error) {
                 console.error('Error fetching gold rate:', error);
                 setGoldRate("error")
@@ -33,7 +36,8 @@ function Gold() {
 
     return (
         <Container>
-            <h1>Today's Gold Rate {GoldRate}</h1>
+            <h1>Today's Gold Rate 22 carat 8g {GoldRate}</h1>
+            <h1>Today's Gold Rate 22 carat 1g {GoldRate_1g}</h1>
         </Container>
     )
 
