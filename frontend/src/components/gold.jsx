99e470fb4a7ae0995da0cd4c2db05decca0ab7ca
mani <itsmanikandanraju@gmail.com>
2024-08-18 01:27:06 +0530

    add date

--- frontend/src/components/gold.jsx
+++ frontend/src/components/gold.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 
 function Gold() {
-
+    const [date, setdate] = useState("")
     const [GoldRate, setGoldRate] = useState("")
     const [GoldRate_1g, setGoldRate_1g] = useState("")
 
@@ -20,8 +20,13 @@ function Gold() {
                 const response = await fetch("https://www.goldapi.io/api/XAU/INR", requestOptions);
                 const res = await response.json();
                 console.log(res);
-                const goldrate = res["price_gram_22k"] * 8
-                const goldrate_1g = res["price_gram_22k"] * 1
+                const goldrate = res["price"] / 3.8879346 
+                const goldrate_1g = res["price"]  / 31.1034768
+                
+                console.log(res["timestamp"])
+                var date = new Date(parseInt(res["timestamp"])*1000);
+                console.log()
+                setdate(date.toDateString())
                 console.log(goldrate);
                 setGoldRate(goldrate)
                 setGoldRate_1g(goldrate_1g)
@@ -36,8 +41,13 @@ function Gold() {
 
     return (
         <Container>
-            <h1>Today's Gold Rate 22 carat 8g {GoldRate}</h1>
-            <h1>Today's Gold Rate 22 carat 1g {GoldRate_1g}</h1>
+            
+            <h1>Today's Gold Rate {date}</h1>
+            <br/>
+            <br/>
+            <h1>1g - {GoldRate_1g}</h1>
+            <h1>8g - {GoldRate}</h1>
+            
         </Container>
     )
 
